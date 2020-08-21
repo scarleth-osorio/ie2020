f7f680a37052895e773d2ccb27f1b16c3fc4d953
hspencer <herbert.spencer@gmail.com>
2020-08-21 15:40:56 -0400

    archivos de la clase
    copié de una carpeta a otra

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(700, 400);
+}
+
+function draw() {
+
+  for(let i = 0; i < 20; i++){
+  // define color aleatorio 
+  stroke(random(255), random(255), random(255));
+  // defino un grosor aleatorio
+  strokeWeight(random(2, 20));
+  // dubujo un punto aleatorio
+  point(random(width), random(height));
+  }
+}
